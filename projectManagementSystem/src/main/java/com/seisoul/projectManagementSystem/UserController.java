023d7664681ffd367b4c3e1bc82a2d09d81eaa4e
CraigL <lgxmx3721@gmail.com>
2023-07-04 16:30:59 +0900

    index page created

---
+++ projectManagementSystem/src/main/java/com/seisoul/projectManagementSystem/UserController.java
@@ -0,0 +1,12 @@
+package com.seisoul.projectManagementSystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    @PostMapping("/membermission")
+    public String memberMission() {
+        return "membermission";
+    }
+}

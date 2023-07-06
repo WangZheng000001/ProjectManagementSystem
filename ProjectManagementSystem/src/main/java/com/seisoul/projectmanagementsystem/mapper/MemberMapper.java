9b16114898b7a3ceb8abe12ee2dca5bcf9488b62
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-07-06 16:47:17 +0900

    ①为接口类MemberMapper增加了多条方法 1.查询所有 2.通过id查询 3.通过姓名查询 4.通过是否是组长查询 5.通过成员状态查询
    ②更新了接口类MemberMapper的注解
    添加注解@Repository,表示改接口是dao层接口
    
    ③为接口的映射文件MemberMapper.xml增加了多条sql语句
    1.查询所有
    2.通过id查询
    3.通过姓名查询
    4.通过是否是组长查询
    5.通过成员状态查询

--- ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/mapper/MemberMapper.java
+++ ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/mapper/MemberMapper.java
@@ -2,8 +2,22 @@ package com.seisoul.projectmanagementsystem.mapper;
 
 import com.seisoul.projectmanagementsystem.pojo.Member;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository    //这是一个Dao层,添加这个注解表示这是一个Dao层
 public interface MemberMapper {
-    Member selectByIdAndPassword(int memId,String memPassword);
+    Member selectByIdAndPassword(int memId, String memPassword);
+
+    Member selectAll(Member member);
+
+    Member selectById(Member member);
+
+    Member selectByName(Member member);
+
+    Member selectByLeader(Member member);
+
+    Member selectByStatus(Member member);
+
+
 }

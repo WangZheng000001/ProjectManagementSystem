856e5d229aca1415ee781398dec78c30428d4d81
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-07-07 19:40:46 +0900

    ①修改了MemberMapper接口类,新增几条接口方法 ②修改了MemberMapper.xml配置文件的sql文

--- ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/mapper/MemberMapper.java
+++ ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/mapper/MemberMapper.java
@@ -6,4 +6,21 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
     Member selectByIdAndPassword(int memId,String memPassword);
+
+    //查询所有成员
+    Member selectAllMember();
+
+    //通过成员id查询
+    Member selectByMemId(Member member);
+
+    //通过成员姓名查询
+    Member selectByMemName(Member member);
+
+    //通过是否是组长查询
+    Member selectByLeader(Member member);
+
+    //通过成员状态
+    Member selectByMemStatus(Member member);
+
+
 }

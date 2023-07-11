af5a8f35bc99ab976e781f8fc6dd0a9e34c0edcb
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-07-12 02:35:12 +0900

    ①修改了MemberMapper 有写方法的返回值写成了单一的对象Member,但当数据库里会查出多行数据的情况下正确的返回值应该是集合List<Member>

--- ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/mapper/MemberMapper.java
+++ ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/mapper/MemberMapper.java
@@ -3,27 +3,30 @@ package com.seisoul.projectmanagementsystem.mapper;
 import com.seisoul.projectmanagementsystem.pojo.Member;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface MemberMapper {
-    Member selectByIdAndPassword(int memId,String memPassword);
 
-    //查询所有成员
-    Member selectAllMember();
-
-    //通过成员id查询
-    Member selectByMemId(Member member);
+    Member selectByIdAndPassword(int memId, String memPassword);
 
     //通过成员姓名查询(散装参数)
     Member selectByMemName(String memName);
 
-    //通过成员姓名查询
+    //方法名:查询所有成员 返回值:Member对象的集合 方法形参:无
+    List<Member> selectAllMember();
+
+    //方法名:通过成员id查询 返回值:Member对象的集合 方法形参:Member对象
+    List<Member> selectByMemId(Member member);
+
+    //方法名:通过成员姓名查询 返回值:一个Member对象 方法形参:Member对象
     Member selectByMemName(Member member);
 
-    //通过是否是组长查询
-    Member selectByLeader(Member member);
+    //方法名:通过是否是组长查询 返回值:Member对象的集合 方法形参:Member对象
+    List<Member> selectByLeader(Member member);
 
-    //通过成员状态
-    Member selectByMemStatus(Member member);
+    //方法名:通过成员状态 返回值:Member对象的集合 方法形参:Member对象
+    List<Member> selectByMemStatus(Member member);
 
 
 }

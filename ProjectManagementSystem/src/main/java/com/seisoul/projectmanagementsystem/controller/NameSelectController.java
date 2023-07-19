d1bd737737d279e5edfedb965d5e5c27ff17dfcf
changcyril40 <136443218+changcyril40@users.noreply.github.com>
2023-07-19 21:53:29 +0900

    解决日期bug

--- ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/controller/NameSelectController.java
+++ ProjectManagementSystem/src/main/java/com/seisoul/projectmanagementsystem/controller/NameSelectController.java
@@ -4,12 +4,15 @@ import com.seisoul.projectmanagementsystem.pojo.Mission;
 import com.seisoul.projectmanagementsystem.pojo.Project;
 import com.seisoul.projectmanagementsystem.service.NameSelectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Controller
@@ -23,12 +26,19 @@ public class NameSelectController {
         model.addAttribute("nameRecord",nameRecord);
         return "namemissioninfo";
     }
-
+    // 定义日期转换器
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
     @RequestMapping("/menbermission1")
     public String menbermission(@RequestParam("memName")String memname,
-                                @RequestParam(value = "startTime", required = false)@DateTimeFormat(pattern = "yyyy-MM-dd")Date startTime,
-                                @RequestParam(value = "startTime", required = false)@DateTimeFormat(pattern = "yyyy-MM-dd")Date endTime,
+                                @RequestParam(name = "startTime", required = false)/*@DateTimeFormat(pattern = "yyyy-MM-dd")*/Date startTime,
+                                @RequestParam(name = "endTime", required = false)/*@DateTimeFormat(pattern = "yyyy-MM-dd")*/Date endTime,
                                 @RequestParam("level")Integer level,Model model){
+        System.out.println(startTime);
         List<Mission> nameRecord = nameSelectService.projectAllByAnyWay(memname, startTime, endTime, level);
         model.addAttribute("nameRecord",nameRecord);
         return "namemissioninfo";
